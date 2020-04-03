0584cd880c59407a457e21b964a10136b6b86230
LENOVO <m.naamneh@gi-group.net>
2020-04-03 23:14:43 +0300

    Update

--- app/src/main/java/com/cds_jo/GalaxySalesApp/assist/dummy/Cls_UpdateData_Adapter.java
+++ app/src/main/java/com/cds_jo/GalaxySalesApp/assist/dummy/Cls_UpdateData_Adapter.java
@@ -19,7 +19,7 @@ import Methdes.MyTextView;
 
 import static com.cds_jo.GalaxySalesApp.R.id.RR1;
 
-//
+//sdsd
 public class Cls_UpdateData_Adapter extends BaseAdapter {
     Context context;
     ArrayList<Cls_UpdateData> cls_updateDatas;
