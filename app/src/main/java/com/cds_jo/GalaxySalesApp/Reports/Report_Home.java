48fb51e7132df950e3881f2b6f903ce161473c33
LENOVO <m.naamneh@gi-group.net>
2020-04-05 11:56:34 +0300

    Update04

---
+++ app/src/main/java/com/cds_jo/GalaxySalesApp/Reports/Report_Home.java
@@ -0,0 +1,15 @@
+package com.cds_jo.GalaxySalesApp.Reports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cds_jo.GalaxySalesApp.R;
+
+public class Report_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report__home);
+    }
+}
